8f4c5ad3d2ecd28ac6d5b8ecf83a72f30c4b0458
rizkydarmadi69 <rizkydarmadi69@gmail.com>
2022-09-29 12:24:07 +0700

    update all

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ import streamlit as st
 st.sidebar.text('')
 st.sidebar.text('')
 st.sidebar.text('')
-st.sidebar.markdown("""## Table of contents
-- [Home](#insight-hasil-tani-indonesia)
-- [Total Hasil tani dari tahun 2000 hingga 2021](#total-hasil-tani-dari-tahun-2000-hingga-2021)
-- [Data Tahunan hasil tani](#data-tahunan-hasil-tani)
-- [Jenis hasil tani terbanyak dari tahun 2000 hingga 2021](#jenis-hasil-tani-terbanyak-dari-tahun-2000-hingga-2021)
+st.sidebar.markdown("""# Table of contents
+- ### [Home](#insight-hasil-tani-indonesia)
+- ### [Total Hasil tani dari tahun 2000 hingga 2021](#total-hasil-tani-dari-tahun-2000-hingga-2021)
+- ### [Data Tahunan hasil tani](#data-tahunan-hasil-tani)
+- ### [Jenis hasil tani terbanyak dari tahun 2000 hingga 2021](#jenis-hasil-tani-terbanyak-dari-tahun-2000-hingga-2021)
                       """)
 
 
@@ -36,4 +36,19 @@ st.bar_chart(top_commodity,y='total',x='jenis_tani',height=450)
 if st.checkbox('Show dataframe#'):
     top_commodity
 
+top_commodity = pd.read_csv('dataset/top_commodity.csv')
+
+st.write('## Jenis hasil tani terbanyak dari tahun 2000 hingga 2021')
+st.bar_chart(top_commodity,y='total',x='jenis_tani',height=450)
+if st.checkbox('Show dataframe#'):
+    top_commodity
+
+import streamlit as st
+import plotly.figure_factory as ff
+import numpy as np
+
+# Add histogram data
+x1 = np.random.randn(200) - 2
+x2 = np.random.randn(200)
+x3 = np.random.randn(200) + 2
 
